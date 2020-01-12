3b152f15906a31b37ce23b7c8348deb47f4d41ec
risonakamo <hmykanae@gmail.com>
2020-01-11 21:14:22 -0500

    operating on all entries output to csv name reduction

--- csvconvert.js
+++ csvconvert.js
@@ -1,4 +1,5 @@
 const fs=require("fs");
+const path=require("path");
 const stringify=require("csv-stringify");
 
 function main()
@@ -6,25 +7,46 @@ function main()
     var logfile=fs.readFileSync("test.log",{encoding:"utf8"}).split("\n");
 
     var res=[];
-    var entryMatch=logfile[0].match(/(.{10}) .{8} (.*)\s/);
-
-    if (!entryMatch)
+    for (var x=0;x<logfile.length;x++)
     {
-        return;
+        var entryMatch=logfile[x].match(/(.{10}) .{8} (.*)/);
+
+        if (!entryMatch)
+        {
+            continue;
+        }
+
+        res.push({
+            date:entryMatch[1],
+            name:simplifyName(entryMatch[2])
+        });
     }
 
-    res.push({
-        date:entryMatch[1],
-        name:entryMatch[2]
+    writeoutCsv(res,"out.csv");
+}
+
+//given a string name, return the simplified version of it, or nothing if it
+//is not the correct file type
+function simplifyName(name)
+{
+    var extension=path.extname(name);
+    if (!(extension==".mkv" || extension==".mp4"))
+    {
+        return "";
+    }
+
+    return name.replace(/\[.*?\]|\.mkv|\.mp4/g,"").replace(/[^\w]|\d/g,"").toLowerCase();
+}
+
+//given data of objects with all the same keys, do a csv write out into the
+//target filename. the data can have any kind of keys as long as its all the same
+//keys
+function writeoutCsv(data,outfile)
+{
+    var outstream=fs.createWriteStream(outfile,{flags:"w"});
+    stringify(data,{columns:Object.keys[data[0]],header:true},(err,output)=>{
+        outstream.write(output);
     });
-
-    console.log(res);
-
-    // var res=[];
-    // for (var x=0;x<logfile.length;x++)
-    // {
-
-    // }
 }
 
 main();
